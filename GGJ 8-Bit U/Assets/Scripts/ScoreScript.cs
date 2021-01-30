064b62073a0dd7f09592801cd0846b4993dc825e
Opus-Jr <71787107+Opus-Jr@users.noreply.github.com>
2021-01-30 17:10:58 -0500

    Score Scirpt

---
+++ GGJ 8-Bit U/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    public Text MyscoreText;
+    private int ScoreNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ScoreNum = 0;
+        MyscoreText.text = "Score : " + ScoreNum;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D Gold)
+    {
+
+        if (Gold.tag == "MyGold")
+        {
+            ScoreNum += 1;
+            Destroy(Gold.gameObject);
+            MyscoreText.text = "Score : " + ScoreNum;
+        }
+
+    }
+}
