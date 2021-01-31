c69ab93485700f31bc4f15516413bb6f62f31daa
Opus-Jr <71787107+Opus-Jr@users.noreply.github.com>
2021-01-31 11:20:53 -0500

    Functional Game
    Ai Causes a Lose Condition

---
+++ GGJ 8-Bit U/Assets/Scripts/PlayerLose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLose : MonoBehaviour
+{
+    public bool LoseCondition = false;
+
+    private void OnTriggerEnter2D(Collider2D Ai)
+    {
+        if (Ai.tag == "Ai")
+            SceneManager.LoadScene(2);
+    }
+}
